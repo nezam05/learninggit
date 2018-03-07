934877f4c903a8bfb085b6dab271b0f0d28a3086
nezam <nezam@thinkpool.net>
2018-03-07 11:30:04 +0600

    deleted redundant file

--- negativeTest.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class negativeTest {
-
-    public static void main(int a[]) {
-        int sum=IntStream.of(a).sum();
-        System.out.println("sum is:"+sum);
-    }
-    
-}
